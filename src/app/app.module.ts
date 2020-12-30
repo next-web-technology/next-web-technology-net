01ee6d8bdafd4d621433b1c6d8586fd9634448af
YasunoriMATSUOKA <yasunori.matsuoka@gmail.com>
2020-12-30 13:40:21 +0900

    Add home page
    close #17

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './page/home/home.component';
+import { ViewHomeComponent } from './view/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ViewHomeComponent,
   ],
   imports: [
     BrowserModule,
