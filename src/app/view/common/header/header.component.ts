07db7ffb9e490c37aa311761b3d966dab7e7c225
YasunoriMATSUOKA <yasunori.matsuoka@gmail.com>
2020-12-30 17:40:19 +0900

    Add header
    close #32

---
+++ src/app/view/common/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'view-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class ViewHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
