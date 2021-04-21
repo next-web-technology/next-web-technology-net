138af85626bdf2a99cc3ba3598eba34f20452f33
YasunoriMATSUOKA <yasunori.matsuoka@gmail.com>
2021-04-21 09:13:58 +0900

    Implement ContactInfrastructureService and firebase functions
    close #69
    
    * Resolve to add HttpClientModule to imports

--- src/app/domain/contact/contact.infrastructure.service.spec.ts
+++ src/app/domain/contact/contact.infrastructure.service.spec.ts
@@ -1,12 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ContactInfrastructureService } from './contact.infrastructure.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('ContactInfrastructureService', () => {
   let service: ContactInfrastructureService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [ContactInfrastructureService],
+      imports: [HttpClientModule],
+    });
     service = TestBed.inject(ContactInfrastructureService);
   });
 
--- src/app/page/contact/contact.component.spec.ts
+++ src/app/page/contact/contact.component.spec.ts
@@ -3,6 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ContactComponent } from './contact.component';
 
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('ContactComponent', () => {
   let component: ContactComponent;
@@ -11,7 +12,7 @@ describe('ContactComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ContactComponent],
-      imports: [MatSnackBarModule],
+      imports: [MatSnackBarModule, HttpClientModule],
     }).compileComponents();
   });
 
