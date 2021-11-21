343ecb8a6d2a9ff1595cdf2561265189c591e727
YasunoriMATSUOKA <yasunori.matsuoka@gmail.com>
2021-11-21 10:39:30 +0900

    feat: post contact message to slack

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,5 @@
 import * as functions from 'firebase-functions';
+import { postSlackMessage } from './utils/slack';
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -12,9 +13,25 @@ export const helloWorld = functions
 
 export const contact = functions
   .region('asia-northeast1')
-  .https.onRequest((request, response) => {
+  .https.onRequest(async (request, response) => {
     try {
       functions.logger.info(request.body, {
+        structureData: true,
+      });
+      const slackMessageText =
+        `email: ${request.body.email}\n` +
+        `name: ${request.body.name}\n` +
+        `title: ${request.body.content}\n` +
+        `content: ${request.body.content}`;
+      const postSlackMessageResult = await postSlackMessage(
+        functions.config().slack.url,
+        slackMessageText,
+        undefined,
+        undefined,
+        undefined,
+        undefined,
+      );
+      functions.logger.info(postSlackMessageResult, {
         structureData: true,
       });
       response.set('Access-Control-Allow-Origin', '*');
@@ -25,7 +42,7 @@ export const contact = functions
       );
       response.json({ data: { status: 'success' } });
     } catch (error) {
-      functions.logger.info(error, { structureData: true });
+      functions.logger.error(error, { structureData: true });
       response.set('Access-Control-Allow-Origin', '*');
       response.set('Access-Control-Allow-Methods', 'GET, HEAD, OPTIONS, POST');
       response.set(
